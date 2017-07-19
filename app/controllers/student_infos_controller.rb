9eba7c05c53e1868aab9f88a2e3cb1a8936ed582
Jean Luc Abayo <luc.bayo@gmail.com>
2017-07-20 01:13:30 +0200

    prepared new create edit and update action in student_infos controller

--- app/controllers/student_infos_controller.rb
+++ app/controllers/student_infos_controller.rb
@@ -24,20 +24,25 @@ class StudentInfosController < ApplicationController
     end
 
     def edit
-      
+      find_student_infos
+      @student = @student_infos.student
     end
 
     def update
-      
+      find_student_infos
+      if ProgramCategory.find_by(id: student_infos_params["program_category_id"]).courses.include?(Course.find_by(id: student_infos_params["course_id"]))
+        @student_infos.update(student_infos_params)
+        flash[:sucess] = "#{@student_infos.student.username} registration Info Updated"
+        redirect_to "/"
+      else
+        redirect_to "/student_infos/#{find_student_infos.id}/edit"
+      end
     end
     private
     def find_student_infos
-      
+      @student_infos = StudentInfo.find_by(id: params[:id])
     end
     def student_infos_params
-      params.require(:student_info).permit(:education_level, :course_id, :program_category_id, :district_id, :registrar_id, :student_id, :country_id)
+      params.require(:student_info).permit(:education_level, :course_id, :program_category_id, :district_id, :registrar_id, :student_id, :country_id, :city)
     end
-    def find_program
-      @program_category = ProgramCategory.find_by(id: 1)
-  end
 end
--- app/models/student_info.rb
+++ app/models/student_info.rb
@@ -1,7 +1,7 @@
 class StudentInfo < ApplicationRecord
   belongs_to :student, class_name: 'User'
   belongs_to :registrar, class_name: 'User'
-  belongs_to :district
+  belongs_to :district, optional: true
   belongs_to :program_category
   belongs_to :country
   belongs_to :course
