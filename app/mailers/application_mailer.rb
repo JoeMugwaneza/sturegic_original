0c495aa9968251e2e15327c4af356ba1c7131158
JoeMugwaneza <muganezajoseph@gmail.com>
2017-07-20 11:48:30 +0200

    pushing other stuff

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'nexpals250@gmail.com'
+  default from: 'from@example.com'
+  layout 'mailer'
   
-  def registration_conformation(user)
-    @user = user
-    mail(:to => "#{user.first_name} <#{user.email}>", :subject => "Registration Conformation")
-  end
 end
