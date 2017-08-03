203da1a886f67259f1d3707a1f79afc33ff379f7
JoeMugwaneza <muganezajoseph@gmail.com>
2017-08-03 12:08:30 +0200

    worked on the routes for student profiles and edit form for profile

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,12 +1,12 @@
 class DashboardsController < ApplicationController
   def index
-   if current_user.admin == true
-    # @users = User.all
-    # @student_infos = StudentInfo.all
-   else
-    @student_info = StudentInfo.find_by(student_id: current_user.id)
-    @associated_registration = current_user.students
-    @user = current_user
-   end
+   # if current_user.admin == true
+   #  # @users = User.all
+   #  # @student_infos = StudentInfo.all
+   # else
+   #  @student_info = StudentInfo.find_by(student_id: current_user.id)
+   #  @associated_registration = current_user.students
+   #  @user = current_user
+   # end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,21 @@ Rails.application.routes.draw do
   resources :student_infos
   resources :users
   resources :password_resets
-   get 'users/:token/confirm_email/', :to => "users#confirm_email", as: 'confirm_email'
-   get 'students/:id', to: 'users#show', as: 'student'
+  get 'users/:token/confirm_email/', :to => "users#confirm_email", as: 'confirm_email'
 
-  # designs
+  get 'students/:id', to: 'users#show', as: 'student'
+
+  get 'students/:id/profile-one', to: 'profiles#profile_one', as: 'student_profile_one'
+
+  get 'students/:id/profile-two', to: "profiles#profile_two", as: 'student_profile_two'
+
+  get 'students/:id/edit', to: 'profiles#edit'
+
   get "application1" => "designs#application1" 
 
   get "application2" => "designs#application2" 
-  get "studentprofile1" => "designs#studentprofile1"
-  get "studentprofile2" => "designs#studentprofile2"
+  # get "studentprofile1" => "designs#studentprofile1"
+  # get "studentprofile2" => "designs#studentprofile2"
   get "admin" => "designs#admin"
   get "agent" => "designs#agent"
 
