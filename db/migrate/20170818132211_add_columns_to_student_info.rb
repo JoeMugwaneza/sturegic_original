b681ad3936526cde6e988718c570b12e1f73001d
JoeMugwaneza <muganezajoseph@gmail.com>
2017-08-18 15:27:31 +0200

    cancled some migration

--- db/migrate/20170818132211_add_columns_to_student_info.rb
+++ db/migrate/20170818132211_add_columns_to_student_info.rb
@@ -1,12 +1,10 @@
-class AddColumnToStudentInfo < ActiveRecord::Migration[5.1]
+class AddColumnsToStudentInfo < ActiveRecord::Migration[5.1]
   def change
     # add_column :student_infos, :district_1, :string
     # add_column :student_infos, :country_1, :string
     # add_column :student_infos, :student_1, :string
     # add_column :student_infos, :registrar_1, :string
-    # add_column :student_infos, :program_1, :string
+    # add_column :student_infos, :program, :string
     # add_column :student_infos, :course_1, :string
-    # add_column :users, :bank, :string
-    # add_column :users, :bank_account, :string
   end
 end
