c3b0ec32ecd3283a7571ccf6fb5098b10024ed7a
plantejosue <plantejosh1@gmail.com>
2017-08-28 11:47:09 +0200

    fixed login responsiveness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820000520) do
+ActiveRecord::Schema.define(version: 20170818132310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,9 +105,9 @@ ActiveRecord::Schema.define(version: 20170820000520) do
     t.string "auth_token"
     t.string "password_reset_token"
     t.datetime "password_reset_sent_at"
-    t.string "identification"
     t.string "registrar_name"
     t.boolean "application_submission", default: false
+    t.string "identification"
     t.string "slug"
     t.boolean "enabled", default: true
     t.string "district"
